815c30e7c0678966262bdd1832d571162a1fffd2
Ihsanul Haque Asif <ihsanul.asif@northsouth.edu>
2023-12-15 14:07:51 +0600

    start c#

---
+++ 1-hello.cs
@@ -0,0 +1,16 @@
+using System;
+
+class HelloWorld {
+    
+  static void Main() {
+    Console.WriteLine("Hello World");
+    Console.WriteLine("Running C#");
+    // This is a comment
+    Console.WriteLine(3+2);
+    /*
+    This is a
+    multiline comment
+    */
+  }
+  
+}
